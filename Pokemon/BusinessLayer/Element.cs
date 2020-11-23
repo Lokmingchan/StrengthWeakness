dace69f441a0e56b65a3bcdf1251193eec62a690
Lok-Ming Chan <74899366+Lokmingchan@users.noreply.github.com>
2020-11-23 04:10:55 -0500

    Add files via upload

---
+++ Pokemon/BusinessLayer/Element.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pokemon.BusinessLayer
+{
+    public class Element
+    {
+        public Element(string Name, 
+            double Normal, double Fire, double Water, double Electric, double Grass, 
+            double Ice, double Fight, double Poison, double Ground, double Flying,
+            double Psychic, double Bug, double Rock, double Ghost, double Dragon,
+            double Dark, double Steel, double Fairy)
+        {
+            ElementName = Name;
+
+            DamageToNormal = Normal;
+            DamageToFire = Fire;
+            DamageToWater = Water;
+            DamageToElectric = Electric;
+            DamageToGrass = Grass;
+
+            DamageToIce = Ice;
+            DamageToFight = Fight;
+            DamageToPoison = Poison;
+            DamageToGround = Ground;
+            DamageToFlying = Flying;
+
+            DamageToPsychic = Psychic;
+            DamageToBug = Bug;
+            DamageToRock = Rock;
+            DamageToGhost = Ghost;
+            DamageToDragon = Dragon;
+
+            DamageToDark = Dark;
+            DamageToSteel = Steel;
+            DamageToFairy = Fairy;
+        }
+
+        public Element(Element e)
+        {
+            ElementName = e.ElementName;
+
+            DamageToNormal = e.DamageToNormal;
+            DamageToFire = e.DamageToFire;
+            DamageToWater = e.DamageToWater;
+            DamageToElectric = e.DamageToElectric;
+            DamageToGrass = e.DamageToGrass;
+
+            DamageToIce = e.DamageToIce;
+            DamageToFight = e.DamageToFight;
+            DamageToPoison = e.DamageToPoison;
+            DamageToGround = e.DamageToGround;
+            DamageToFlying = e.DamageToFlying;
+
+            DamageToPsychic = e.DamageToPsychic;
+            DamageToBug = e.DamageToBug;
+            DamageToRock = e.DamageToRock;
+            DamageToGhost = e.DamageToGhost;
+            DamageToDragon = e.DamageToDragon;
+
+            DamageToDark = e.DamageToDark;
+            DamageToSteel = e.DamageToSteel;
+            DamageToFairy = e.DamageToFairy;
+        }
+
+        public double GetDamage(int ElementIndex)
+        {
+            switch (ElementIndex)
+            {
+                case 1:
+                    return DamageToNormal;
+                case 2:
+                    return DamageToFire;
+                case 3:
+                    return DamageToWater;
+                case 4:
+                    return DamageToElectric;
+                case 5:
+                    return DamageToGrass;
+                case 6:
+                    return DamageToIce;
+                case 7:
+                    return DamageToFight;
+                case 8:
+                    return DamageToPoison;
+                case 9:
+                    return DamageToGround;
+                case 10:
+                    return DamageToFlying;
+                case 11:
+                    return DamageToPsychic;
+                case 12:
+                    return DamageToBug;
+                case 13:
+                    return DamageToRock;
+                case 14:
+                    return DamageToGhost;
+                case 15:
+                    return DamageToDragon;
+                case 16:
+                    return DamageToDark;
+                case 17:
+                    return DamageToSteel;
+                case 18:
+                    return DamageToFairy;
+                default:
+                    return 1;
+            }
+        }
+
+        public double GetDamage(string ElementName)
+        {
+            switch (ElementName)
+            {
+                case "Normal":
+                    return DamageToNormal;
+                case "Fire":
+                    return DamageToFire;
+                case "Water":
+                    return DamageToWater;
+                case "Electric":
+                    return DamageToElectric;
+                case "Grass":
+                    return DamageToGrass;
+                case "Ice":
+                    return DamageToIce;
+                case "Fight":
+                    return DamageToFight;
+                case "Poison":
+                    return DamageToPoison;
+                case "Ground":
+                    return DamageToGround;
+                case "Flying":
+                    return DamageToFlying;
+                case "Psychic":
+                    return DamageToPsychic;
+                case "Bug":
+                    return DamageToBug;
+                case "Rock":
+                    return DamageToRock;
+                case "Ghost":
+                    return DamageToGhost;
+                case "Dragon":
+                    return DamageToDragon;
+                case "Dark":
+                    return DamageToDark;
+                case "Steel":
+                    return DamageToSteel;
+                case "Fairy":
+                    return DamageToFairy;
+                default:
+                    return 1;
+            }
+        }
+
+        public string ElementName { get; set; }
+
+        public double DamageToNormal { get; set; }
+        public double DamageToFire { get; set; }
+        public double DamageToWater { get; set; }
+        public double DamageToElectric { get; set; }
+        public double DamageToGrass { get; set; }
+
+        public double DamageToIce { get; set; }
+        public double DamageToFight { get; set; }
+        public double DamageToPoison { get; set; }
+        public double DamageToGround { get; set; }
+        public double DamageToFlying { get; set; }
+
+        public double DamageToPsychic { get; set; }
+        public double DamageToBug { get; set; }
+        public double DamageToRock { get; set; }
+        public double DamageToGhost { get; set; }
+        public double DamageToDragon { get; set; }
+
+        public double DamageToDark { get; set; }
+        public double DamageToSteel { get; set; }
+        public double DamageToFairy { get; set; }
+
+    }
+}
---
+++ Pokemon/BusinessLayer/Elements.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pokemon.BusinessLayer
+{
+    public class Elements
+    {
+        public Elements()
+        {
+            PokemonElements = new Element[19];
+
+            PokemonElements[0] = MissingNo;
+
+            PokemonElements[1] = Normal;
+            PokemonElements[2] = Fire;
+            PokemonElements[3] = Water;
+            PokemonElements[4] = Electric;
+            PokemonElements[5] = Grass;
+
+            PokemonElements[6] = Ice;
+            PokemonElements[7] = Fight;
+            PokemonElements[8] = Poison;
+            PokemonElements[9] = Ground;
+            PokemonElements[10] = Flying;
+
+            PokemonElements[11] = Psychic;
+            PokemonElements[12] = Bug;
+            PokemonElements[13] = Rock;
+            PokemonElements[14] = Ghost;
+            PokemonElements[15] = Dragon;
+
+            PokemonElements[16] = Dark;
+            PokemonElements[17] = Steel;
+            PokemonElements[18] = Fairy;
+            
+        }
+
+        //public enum ElementNames
+        //{
+        //    Normal = 1, Fire = 2, Water = 3, Electric = 4, Grass = 5,
+        //    Ice = 6, Fight = 7, Poison = 8, Ground = 9, Flying = 10,
+        //    Psychic = 11, Bug = 12, Rock = 13, Ghost = 14, Dragon = 15,
+        //    Dark = 16, Steel = 17, Fairy = 18
+        //};
+
+        public Element[] PokemonElements
+        {
+            get;
+            set;
+        }
+
+        public Element GetElement(string ElementName)
+        {
+            foreach (Element e in PokemonElements)
+            {
+                if (e.ElementName == ElementName)
+                {
+                    return e;
+                }
+            }
+
+            return MissingNo;
+        }
+
+        private Element Normal = new Element("Normal",
+            1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1,
+            1, 1, .5, 0, 1,
+            1, .5, 1);
+
+        private Element Fire = new Element("Fire",
+            1, .5, .5, 1, 2,
+            2, 1, 1, 1, 1,
+            1, 2, .5, 1, .5,
+            1, 2, 1);
+
+        private Element Water = new Element("Water",
+            1, 2, .5, 1, .5,
+            1, 1, 1, 2, 1,
+            1, 1, 2, 1, .5,
+            1, 1, 1);
+
+        private Element Electric = new Element("Electric",
+            1, 1, 2, .5, .5,
+            1, 1, 1, 0, 2,
+            1, 1, 1, 1, .5,
+            1, 1, 1);
+
+        private Element Grass = new Element("Grass",
+            1, .5, 2, 1, .5,
+            1, 1, .5, 2, .5,
+            1, .5, 2, 1, .5,
+            1, .5, 1);
+
+        private Element Ice = new Element("Ice",
+            1, .5, .5, 1, 2,
+            .5, 1, 1, 2, 2,
+            1, 1, 1, 1, 2,
+            1, .5, 1);
+
+        private Element Fight = new Element("Fight",
+            2, 1, 1, 1, 1,
+            2, 1, .5, 1, .5,
+            .5, .5, 2, 0, 1,
+            2, 2, .5);
+
+        private Element Poison = new Element("Poison",
+            1, 1, 1, 1, 2,
+            1, 1, .5, .5, 1,
+            1, 1, .5, .5, 1,
+            1, 0, 2);
+
+        private Element Ground = new Element("Ground",
+            1, 2, 1, 2, .5,
+            1, 1, 2, 1, 0,
+            1, .5, 2, 1, 1,
+            1, 2, 1);
+
+        private Element Flying = new Element("Flying",
+            1, 1, 1, .5, 2,
+            1, 2, 1, 1, 1,
+            1, 2, .5, 1, 1,
+            1, .5, 1);
+
+        private Element Psychic = new Element("Psychic",
+            1, 1, 1, 1, 1,
+            1, 2, 2, 1, 1,
+            .5, 1, 1, 1, 1,
+            0, .5, 1);
+
+        private Element Bug = new Element("Bug",
+            1, .5, 1, 1, 2,
+            1, .5, .5, 1, .5,
+            2, 1, 1, .5, 1,
+            2, .5, .5);
+
+        private Element Rock = new Element("Rock",
+            1, 2, 1, 1, 1,
+            2, .5, 1, .5, 2,
+            1, 2, 1, 1, 1,
+            1, .5, 1);
+
+        private Element Ghost = new Element("Ghost",
+            0, 1, 1, 1, 1,
+            1, 1, 1, 1, 1,
+            2, 1, 1, 2, 1,
+            .5, 1, 1);
+
+        private Element Dragon = new Element("Dragon",
+            1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1,
+            1, 1, 1, 1, 2,
+            1, .5, 0);
+
+        private Element Dark = new Element("Dark",
+            1, 1, 1, 1, 1,
+            1, .5, 1, 1, 1,
+            2, 1, 1, 2, 1,
+            .5, 1, .5);
+
+        private Element Steel = new Element("Steel",
+            1, .5, .5, .5, 1,
+            2, 1, 1, 1, 1,
+            1, 1, 2, 1, 1,
+            1, .5, 2);
+
+        private Element Fairy = new Element("Fairy",
+            1, .5, 1, 1, 1,
+            1, 2, .5, 1, 1,
+            1, 1, 1, 1, 2,
+            2, .5, 1);
+
+        private Element MissingNo = new Element("MissingNo",
+            1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1,
+            1, 1, 1);
+    }
+}
